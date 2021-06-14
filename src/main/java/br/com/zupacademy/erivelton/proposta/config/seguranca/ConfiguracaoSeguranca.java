f85aaad88527367efaefe3528a5c490d982af088
erivelton-paulista@hotmail.com <erivelton-paulista@hotmail.com>
2021-06-13 22:19:06 -0300

    Integrando autenticação com API externa

---
+++ src/main/java/br/com/zupacademy/erivelton/proposta/config/seguranca/ConfiguracaoSeguranca.java
@@ -0,0 +1,24 @@
+package br.com.zupacademy.erivelton.proposta.config.seguranca;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+
+public class ConfiguracaoSeguranca extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.and()
+				.authorizeRequests().anyRequest()
+				.authenticated()
+				.and()
+				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+	}
+
+}
